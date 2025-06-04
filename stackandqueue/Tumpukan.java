c5f1e246186e21ae7d7d2f4d27802b74858e538c
azmttqi3 <azmiittaqi03@gmail.com>
2025-06-04 08:02:07 +0700

    tumpukan barang

---
+++ stackandqueue/Tumpukan.java
@@ -0,0 +1,37 @@
+public class Tumpukan {
+    Barang palingAtas;
+    
+    public void tumpuk(Barang b){
+        b.dibawahnya = palingAtas;
+        palingAtas = b;
+    }
+
+    public void lihatIsiTumpukan(){
+        if(palingAtas == null){
+            System.out.println("Tumpukan Kosong");
+            return;
+
+        }
+
+        Barang sekarang = palingAtas;
+        System.out.println("Barang : " +sekarang.nama);
+
+        while(sekarang.dibawahnya != null){
+            sekarang = sekarang.dibawahnya;
+            System.out.println("Barang : "+sekarang.nama);
+        }
+    }
+
+    public Barang ambil(){
+        return null;
+    }
+
+    public Barang lihatPalingAtas(){
+        return null;
+    }
+
+    public Boolean kososngkah(){
+        
+        return null;
+    }
+}
